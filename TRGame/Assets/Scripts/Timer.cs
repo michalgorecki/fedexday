e6690405232bb6f61c3386855558344793866590
michalgorecki <mih.gorecki@gmail.com>
2017-12-20 09:51:41 +0100

    Pierwsza działająca wersja timera

---
+++ TRGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+	private string timerMessage = "Left from WLA: ";
+	private bool WLA_MISSED = false;
+	float timer = 5.0f;
+
+	void Update()
+	{
+		if (timer < 0 && !WLA_MISSED) {
+			WLA_MISSED = true;
+			timerMessage = "Missed WLA: ";
+		}
+		timer = timer - Time.deltaTime;
+
+	}
+
+	void OnGUI()
+	{
+		GUI.Box (new Rect (100, 100, 300, 50), timerMessage + (int)timer);
+	}
+}
